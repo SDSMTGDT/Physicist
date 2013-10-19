5e881f515a7255574f8ff3e48d8799c8e45ac19b
Porkenstein <portenstein@gmail.com>
2013-10-19 14:24:22 -0600

    Added Farseer Engine, Cleaned up some classes and reorganized namespaces

---
+++ Physicist/Physicist/Actors/Fields/IField.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Physicist.Actors.Fields
+{
+    interface IField
+    {
+        public float Direction; //angle in radians
+        public uint Magnitude; //magnitude of the affecting vector
+
+        //sprite is a back color, translucent - > to be used with Actor
+
+        //overloading
+        public void Draw();
+        public void AffectPlayer(Player p);
+
+    }
+}
