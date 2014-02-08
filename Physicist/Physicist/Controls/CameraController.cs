15cf22c34600fbdb402b1a9f52c9146689b62930
Porkenstein <portenstein@gmail.com>
2014-02-08 16:56:03 -0700

    Added Camera functions and properties, using the Viewport

--- Physicist/Physicist/Controls/CameraController.cs
+++ Physicist/Physicist/Controls/CameraController.cs
@@ -4,8 +4,128 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
+    using Microsoft.Xna.Framework;
+    using Physicist.Actors;
+    using Physicist.Extensions;
 
     public class CameraController
     {
+        private Matrix originScaleRotationMatrix;
+        
+        private Matrix originMatrix;
+        
+        private Matrix scaleMatrix;
+        
+        private Matrix rotationMatrix;
+        
+        private Vector2 origin;
+        
+        private float zoom;
+        
+        private float rotation;
+
+        public CameraController()
+        {
+            this.Following = null;
+            this.Zoom = 1;
+            this.Position = Vector2.Zero;
+            this.Rotation = 0;
+            this.Origin = Vector2.Zero;
+        }
+
+        public CameraController(Vector2 position, Vector2 origin, Actor following, float zoom, float rotation)
+        {
+            this.Following = following;
+            this.Zoom = zoom;
+            this.Position = position;
+            this.Rotation = rotation;
+            this.Origin = origin;
+        }
+
+        public Viewport CameraViewport { get; set; }
+        
+        public Actor Following { get; set; }
+
+        public float Zoom
+        { 
+            get
+            {
+                return this.zoom;
+            } 
+
+            set
+            {
+                this.zoom = value;
+                this.scaleMatrix = Matrix.CreateScale(new Vector3(this.Zoom, this.Zoom, 1));
+                this.originScaleRotationMatrix = this.rotationMatrix * this.scaleMatrix * this.originMatrix;
+            }
+        }
+        
+        public Vector2 Position { get; set; }
+
+        public float Rotation
+        {
+            get
+            {
+                return this.rotation;
+            } 
+
+            set
+            {
+                this.rotation = value;
+                this.rotationMatrix = this.rotationMatrix = Matrix.CreateRotationZ(this.Rotation);
+                this.originScaleRotationMatrix = this.rotationMatrix * this.scaleMatrix * this.originMatrix;
+            }
+        }
+
+        public Vector2 Origin
+        {
+            get
+            {
+                return this.origin;
+            }
+            
+            set
+            {
+                this.origin = value;
+                this.originMatrix = Matrix.CreateTranslation(new Vector3(this.Origin.X, this.Origin.Y, 0));
+                this.originScaleRotationMatrix = this.rotationMatrix * this.scaleMatrix * this.originMatrix;
+            }
+        }
+
+        public Matrix Transform
+        {
+            get
+            {
+                var translationMatrix = Matrix.CreateTranslation(new Vector3(this.Position.X, this.Position.Y, 0));
+                return translationMatrix * this.originScaleRotationMatrix;
+            }
+        }
+
+        public void Move(Vector2 direction)
+        {
+            this.Position += direction;
+        }
+
+        public void Rotate(float angle)
+        {
+            this.Rotation += angle;
+        }
+
+        public void ZoomIn(float magnitude)
+        {
+            this.Zoom += magnitude;
+        }
+
+        public void ZoomOut(float magnitude)
+        {
+            this.Zoom -= magnitude;
+        }
+
+        public void CenterOnFollowing()
+        {
+            // Define the camera's position as centered on the player (or other object, if so desired)
+            this.Position = new Vector2(Math.Max(0, this.Following.Position.X - (this.CameraViewport.ViewportSize.Width / 2)), Math.Max(0, this.Following.Position.Y - (this.CameraViewport.ViewportSize.Height / 2)));
+        }
     }
 }
--- Physicist/Physicist/Controls/Viewport.cs
+++ Physicist/Physicist/Controls/Viewport.cs
@@ -4,8 +4,17 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
+    using Physicist.Extensions;
 
     public class Viewport
     {
+        // for now, just holds the width and height of the viewport.
+        // may in the future interact with the graphcis device or attempt to get viewport information based on the game or the screen
+        public Viewport(Size viewportSize)
+        {
+            this.ViewportSize = viewportSize;
+        }
+
+        public Size ViewportSize { get; set; }
     }
 }
